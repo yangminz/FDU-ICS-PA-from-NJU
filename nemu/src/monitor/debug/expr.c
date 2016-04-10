bf1d25f4067c0cc23de6719ec0e5fe7b67ee237e
yangminz <yangminz@outlook.com>
2016-04-10 11:39:24 +0800

    Added files via upload
    complete the regex token recognition

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <regex.h>
 
+#include <malloc.h>
+
 enum {
-	NOTYPE = 256, EQ
+	NOTYPE = 256, EQ , DEC = 23
 
 	/* TODO: Add more token types */
 
@@ -21,9 +23,14 @@ static struct rule {
 	/* TODO: Add more rules.
 	 * Pay attention to the precedence level of different rules.
 	 */
-
+	 {"[0-9]{1,}",	DEC},
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
+	{"\\-", '-'},
+	{"\\*", '*'},
+	{"\\/", '/'},
+	{"\\(", '('},
+	{"\\)", ')'},
 	{"==", EQ}						// equal
 };
 
@@ -55,17 +62,21 @@ typedef struct token {
 
 Token tokens[32];
 int nr_token;
+// used for the whole codeevaluate 
 
 static bool make_token(char *e) {
 	int position = 0;
 	int i;
 	regmatch_t pmatch;
 	
-	nr_token = 0;
+	nr_token = 0; 
 
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
+			// #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+			// regexec is a function from Regular Expression library
+			// to match regular expression
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
@@ -77,11 +88,39 @@ static bool make_token(char *e) {
 				 * to record the token in the array ``tokens''. For certain 
 				 * types of tokens, some extra actions should be performed.
 				 */
-
 				switch(rules[i].token_type) {
+					case NOTYPE: break;
+					case DEC:
+						tokens[nr_token].type = DEC;
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;	
+					case '+':
+						tokens[nr_token].type = '+';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
+					case '-':
+						tokens[nr_token].type = '-';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
+					case '*':
+						tokens[nr_token].type = '*';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
+					case '/':
+						tokens[nr_token].type = '/';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
+					case '(':
+						tokens[nr_token].type = '(';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
+					case ')':
+						tokens[nr_token].type = ')';
+						sprintf( tokens[nr_token].str, "%.*s", substr_len, substr_start );
+						break;
 					default: panic("please implement me");
 				}
-
+				nr_token += 1;
 				break;
 			}
 		}
@@ -100,8 +139,11 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-
+	int i;
+	for( i = 0; i < 32; i ++ )
+		printf("%s", tokens[i].str );
 	/* TODO: Insert codes to evaluate the expression. */
+	// use tokens[] to evaluate
 	panic("please implement me");
 	return 0;
 }
