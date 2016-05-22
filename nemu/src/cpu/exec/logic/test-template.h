638f32dfaf3a543ab4a1b610b2a9e23b6e9c3ffd
yangminz <yangminz@users.noreply.github.com>
2016-05-22 15:01:31 +0800

    test instruction

---
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -0,0 +1,33 @@
+#include "cpu/exec/template-start.h"
+
+#define instr test
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val & op_src->val;
+	uint8_t low_order_8_bits = result & 0xff;
+	int parity = 1;
+	int i = 0;
+	//printf("result: 0x%x\n", result);
+
+	cpu.eflags.CF = 0;
+	for(i=0;i<8;i++){
+		if(low_order_8_bits & 0x1){
+			parity = !parity;
+		}
+		low_order_8_bits = low_order_8_bits >> 1;
+	}
+	cpu.eflags.PF = parity;
+	/*AF is undefined*/
+	cpu.eflags.ZF = !result;
+	cpu.eflags.SF = MSB(result);
+	cpu.eflags.OF = 0;
+	//panic("please implement me");
+
+	print_asm_template2();
+}
+
+make_instr_helper(r2rm)
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+
+#include "cpu/exec/template-end.h"
