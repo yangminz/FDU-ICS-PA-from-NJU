33ac2496aa67979b01014cf9143064eba50d0690
yangminz <yangminz@users.noreply.github.com>
2016-05-22 14:58:16 +0800

    implement more instructions

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -2,6 +2,10 @@
 
 #include "data-mov/mov.h"
 #include "data-mov/xchg.h"
+/****************************************/
+/* IMPLEMENTED INSTRUCTIONS!            */
+/****************************************/
+#include "data-mov/push.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
@@ -10,6 +14,11 @@
 #include "arith/mul.h"
 #include "arith/idiv.h"
 #include "arith/div.h"
+/****************************************/
+/* IMPLEMENTED INSTRUCTIONS!            */
+/****************************************/
+#include "arith/sub.h"
+#include "arith/cmp.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
@@ -19,6 +28,16 @@
 #include "logic/shl.h"
 #include "logic/shr.h"
 #include "logic/shrd.h"
+/****************************************/
+/* IMPLEMENTED INSTRUCTIONS!            */
+/****************************************/
+#include "logic/test.h"
+
+/****************************************/
+/* IMPLEMENTED INSTRUCTIONS!            */
+/****************************************/
+#include "ctrl-transfer/call.h"
+#include "ctrl-transfer/jcc/je.h"
 
 #include "string/rep.h"
 
